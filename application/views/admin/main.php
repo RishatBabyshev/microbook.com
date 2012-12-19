77882ea15705a6a28faceef8843fc0eed1686876
RishatBabyshev <b.rishat@gmail.com>
2012-12-19 20:09:01 +0600

    Some addons

--- application/views/admin/main.php
+++ application/views/admin/main.php
@@ -9,8 +9,58 @@
  
     <div id="right_wrap">
 		<div id="right_content">             
-			<h2>Quick icons</h2> 
-	 
+			<h2>Quick icons</h2>
+			<br />
+			<div class="cpanel">
+				<div class="icon-wrapper">
+					<div class="icon">
+						<a href="<?php echo site_url('admin/article_add');?>">
+							<img src="<?php echo base_url();?>images/quick-icon/article-add.png" alt="">
+							<span>Add New Article</span>
+						</a>
+					</div>
+				</div>
+				<div class="icon-wrapper">
+					<div class="icon">
+						<a href="<?php echo site_url('admin/article_list');?>">
+							<img src="<?php echo base_url();?>images/quick-icon/article-list.png" alt="">
+							<span>Article Manager</span>
+						</a>
+					</div>
+				</div>
+				<div class="icon-wrapper">
+					<div class="icon">
+						<a href="<?php echo site_url('admin/category_list');?>">
+							<img src="<?php echo base_url();?>images/quick-icon/category-list.png" alt="">
+							<span>Category Manager</span>
+						</a>
+					</div>
+				</div>
+				<div class="icon-wrapper">
+					<div class="icon">
+						<a href="#">
+							<img src="<?php echo base_url();?>images/quick-icon/user.png" alt="">
+							<span>User Manager</span>
+						</a>
+					</div>
+				</div>
+				<div class="icon-wrapper">
+					<div class="icon">
+						<a href="#">
+							<img src="<?php echo base_url();?>images/quick-icon/config.png" alt="">
+							<span>Settings</span>
+						</a>
+					</div>
+				</div>
+				<div class="icon-wrapper">
+					<div class="icon">
+						<a href="#">
+							<img src="<?php echo base_url();?>images/quick-icon/help.png" alt="">
+							<span>Help</span>
+						</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div><!-- end of right content-->
                      
